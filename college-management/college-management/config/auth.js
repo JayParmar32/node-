ecd84613efad90e6ed98cbb25a3314f3467c8fef
Jay3206 <jayparmar9879@gmail.com>
2026-04-16 20:41:46 +0530

    Delete college-management/college-management directory

--- college-management/college-management/config/auth.js
+++
@@ -1,5 +0,0 @@
-module.exports.isAuthenticated = (req, res, next) => {
-  if (req.session.admin) return next();
-  req.flash('error_msg', 'Please login to access the admin panel');
-  res.redirect('/login');
-};
